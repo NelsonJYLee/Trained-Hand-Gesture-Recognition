878ecea9965861d895bf607fff4037597ad20cc5
NelsonLee2000 <nelsonleex@gmail.com>
2025-04-28 16:21:26 -0700

    train_classifier and inference_classifier finished with normalization

--- create_dataset.py
+++ create_dataset.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 
 import mediapipe as mp
 import cv2
@@ -13,16 +14,18 @@ hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 DATA_DIR = './data'
 
 data = []
+norm_data = []
 labels = []
 for dir_ in os.listdir(DATA_DIR):
     if dir_ == ".DS_Store":
             continue
     
-    for img_path in os.listdir(os.path.join(DATA_DIR, dir_))[:1]:
+    for img_path in os.listdir(os.path.join(DATA_DIR, dir_)):
         if img_path == ".DS_Store":
             continue
 
         data_aux = []
+        norm_data_aux = []
 
         img = cv2.imread(os.path.join(DATA_DIR, dir_, img_path))
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -33,15 +36,24 @@ for dir_ in os.listdir(DATA_DIR):
                 for i in range(len(hand_landmarks.landmark)):
                     x = hand_landmarks.landmark[i].x
                     y = hand_landmarks.landmark[i].y
+
+                    norm_x = x - hand_landmarks.landmark[0].x
+                    norm_y = y - hand_landmarks.landmark[0].y
+
                     data_aux.append(x)
                     data_aux.append(y)
 
+                    norm_data_aux.append(norm_x)
+                    norm_data_aux.append(norm_y)
+
             data.append(data_aux)
+            norm_data.append(norm_data_aux)
             labels.append(dir_)
-            
-                      
 
-        plt.figure()
-        plt.imshow(img_rgb)
+f = open('data.pickle', 'wb')
+pickle.dump({'data': data, 'labels': labels}, f)
+f.close()
 
-plt.show()
+norm_f = open('norm_data.pickle', 'wb')
+pickle.dump({'data': norm_data, 'labels': labels}, norm_f)
+norm_f.close()
